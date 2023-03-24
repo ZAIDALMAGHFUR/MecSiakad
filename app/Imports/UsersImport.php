0a37e3137c5c5e4f49dba9c06fc3c41c7eb1209c
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2023-03-24 13:23:12 +0700

    clear add fitur import mahasiswa

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -4,9 +4,11 @@ namespace App\Imports;
 
 use App\Models\User;
 use App\Models\Mahasiswa;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\ToModel;
 
-class UsersImport implements ToModel
+class UsersImport implements ToCollection
 {
     /**
     * @param array $row
@@ -14,30 +16,43 @@ class UsersImport implements ToModel
     * @return \Illuminate\Database\Eloquent\Model|null
     */
 
-
-    public function model(array $row)
+    public function model(array $rows)
     {
-        $user = User::create([
-            'username' => $row[0],
-            'email' => $row[1],
-            'password' => bcrypt($row[2]),
-            'roles_id' => $row[3],
-        ]);
+        
+    }
+    public function collection(Collection $rows)
+    {
+        // dd($rows);
+        foreach($rows as $key=>$row){
+            if($key === 0){
+                continue;
+            }
+            // dd($row[2]);
+            
 
-        return new Mahasiswa([
-                'name' => $row['name'],
-                'nim' => $row['nim'],
-                'email' => $row['email'],
-                'no_hp' => $row['no_hp'],
-                'alamat' => $row['alamat'],
-                'program_studies_id' => $row['program_studies_id'],
-                'tempat_lahir' => $row['tempat_lahir'],
-                'tanggal_lahir' => $row['tanggal_lahir'],
-                'jenis_kelamin' => $row['jenis_kelamin'],
-                'agama' => $row['agama'],
-                'status' => $row['status'],
-                'user_id' => $user->id,
-                'foto' => $row['foto'],
-        ]);
+            $user = User::create([
+                'username' => $row[0],
+                'email' => $row[2],
+                'password' => bcrypt($row[7]),
+                'roles_id' => 3,
+            ]);
+            
+            Mahasiswa::create([
+                    'name' => $row[0],
+                    'nim' => $row[1],
+                    'email' => $row[2],
+                    'no_hp' => $row[3],
+                    'alamat' => $row[4],
+                    'program_studies_id' => $row[5],
+                    'tempat_lahir' => $row[6],
+                    'tanggal_lahir' => $row[7],
+                    'jenis_kelamin' => $row[8],
+                    'agama' => $row[9],
+                    'status' => $row[10],
+                    'user_id' => $user->id,
+                    'foto' => $row[12] ?? '',
+            ]);
+        }
+
     }
 }
