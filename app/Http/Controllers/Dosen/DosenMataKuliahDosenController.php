ea038c02a8fd44f7ed7a74ebc649622d83b21b8f
Zaid al maghfur <91768583+ZAIDALMAGHFUR@users.noreply.github.com>
2023-04-13 21:12:26 +0700

    dosen matkul update

--- app/Http/Controllers/Dosen/DosenMataKuliahDosenController.php
+++ app/Http/Controllers/Dosen/DosenMataKuliahDosenController.php
@@ -15,31 +15,7 @@ class DosenMataKuliahDosenController extends Controller
     public function index()
     {
         $dosen = Dosen::Where('users_id', Auth::user()->id)->first();
-         //dd($dosen->dosenJabatans());
-        $dosen_jabatan = $dosen->dosenJabatans()->first();
-        if ($dosen_jabatan->jabatan_id == '1') {
-            $dsnmatkul = Mata_Kuliah::where('program_studies_id', $dosen_jabatan->program_studies_id)->get();
-        } else {
-            $dsnmatkul = DosenMatkul::Where('dosen_id', $dosen->id)->get();
-        }
+        $dsnmatkul = DosenMatkul::Where('dosen_id', $dosen->id)->get();
         return view('dashboard.dosen.data-matkul.index', compact('dsnmatkul'));
     }
-
-//     public function index()
-// {
-//     $dosen = Dosen::where('users_id', Auth::user()->id)->first();
-//     // dd($dosen->dosenJabatans);
-//     $dsnmatkul = [];
-
-//     if ($dosen) {
-//         $dosenJabatan = $dosen->dosenJabatans()->first();
-        
-//         if ($dosenJabatan && $dosenJabatan->jabatan_id == '1') {
-//             $dsnmatkul = DosenMatkul::where('dosen_id', $dosen->id)->get();
-//         }
-//     }
-
-//     return view('dashboard.dosen.data-matkul.index', compact('dsnmatkul'));
-// }
-
 }
